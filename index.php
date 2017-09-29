fd0d33b84a47866de793fb0693efac80e70667ad
AndreVictor60 <andre.victor60@free.fr>
2017-09-29 14:28:48 +0200

    Écriture de l'exercice 4

---
+++ index.php
@@ -0,0 +1,13 @@
+  <?php
+for($i = 0; $i <= 100; $i++){
+  if($i % 3 == 0):
+    echo $i." est une multiple de 3 (Fizz)<br />";
+
+    elseif ($i % 5 == 0):
+      echo $i." est un multiple de 5 (Buzz) <br />";
+
+      elseif ($i % 3 == 0 && $i % 5 == 0):
+        echo $i." est multiple de 3 et 5 (FizzBuzz) <br />";
+endif;
+}
+?>
