5164052687a151bb80eae7d072253bcd95ce9c5b
AndreVictor60 <andre.victor60@free.fr>
2017-10-02 10:03:48 +0200

    Ajout des commentaires

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
-  <?php
-for($i = 0; $i <= 100; $i++){
-  if($i % 3 == 0 && $i % 5 == 0):
-    echo $i." est multiple de 3 et 5 (FizzBuzz)<br />";
-
-    elseif ($i % 5 == 0):
-      echo $i." est un multiple de 5 (Buzz) <br />";
-
-      elseif ($i % 3 == 0):
-        echo $i." est multiple de 3 (Fizz) <br />";
-endif;
-}
+<?php
+// $i est init a 0, $i jusqu'a 100, $i ajout +1 chaque tour
+  for($i = 0; $i <= 100; $i++){
+    // Si $i est multiple de 3 et $i est multiple de 5 on affiche FizzBuzz
+    if($i % 3 == 0 && $i % 5 == 0):
+      echo $i." est multiple de 3 et 5 (FizzBuzz)<br />";
+      //Sinon si $i est multiple de 5 on affiche Buzz
+      elseif ($i % 5 == 0):
+        echo $i." est un multiple de 5 (Buzz) <br />";
+        //Sinon si $i est multiple de 3 on affice Fizz
+        elseif ($i % 3 == 0):
+          echo $i." est multiple de 3 (Fizz) <br />";
+  //Fin du Si
+  endif;
+  }
 ?>
