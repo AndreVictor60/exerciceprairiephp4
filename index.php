4b157ff2f95395e1346c3a4d031c745431c48a8f
AndreVictor60 <andre.victor60@free.fr>
2017-10-03 10:11:40 +0200

    Ajout des ''

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
   for($i = 1; $i <= 100; $i++){
     // Si $i est multiple de 3 et $i est multiple de 5 on affiche FizzBuzz
     if($i % 3 == 0 && $i % 5 == 0):
-      echo $i." est multiple de 3 et 5 (FizzBuzz)<br />";
+      echo $i.' est multiple de 3 et 5 (FizzBuzz)<br />';
       //Sinon si $i est multiple de 5 on affiche Buzz
       elseif ($i % 5 == 0):
-        echo $i." est un multiple de 5 (Buzz) <br />";
+        echo $i.' est un multiple de 5 (Buzz) <br />';
         //Sinon si $i est multiple de 3 on affice Fizz
         elseif ($i % 3 == 0):
-          echo $i." est multiple de 3 (Fizz) <br />";
+          echo $i.' est multiple de 3 (Fizz) <br />';
   //Fin du Si
   endif;
   }
